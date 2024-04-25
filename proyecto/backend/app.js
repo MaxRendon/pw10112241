1b25e69aa163ed6975c6c7e1cea9dcaf022fa3a8
Maximiliano Rendon <119466895+MaxRendon@users.noreply.github.com>
2024-04-25 10:50:07 -0700

    Conexion BDD
    Conexion BDD

--- proyecto/backend/app.js
+++ proyecto/backend/app.js
@@ -6,10 +6,37 @@ let app = express();
 app.use(express.json());
 app.use(cors());
 
+//Conexion a MySQL
+let conexion = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'pwdata',
+    port:   '3306' 
+});
+//Nos conectamos a MySQL
+conexion.connect(function(error){
+    if(error){
+        throw error;
+    }else{
+        console.log('Conextando a la base de datos');
+    }
+})
+
 //Rutas de acceso
 app.get("/",function(req,res){
     res.send("Ruta de inicios");
 });
+//Seleccionamos todos los clientes
+app.get('/api/clientes',(req,res)=>{
+    conexion.query("SELECT * FROM clientes",(error,filas)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(filas);
+        }
+    });
+})
 
 //Encender servidor
 let puerto = 3000;
