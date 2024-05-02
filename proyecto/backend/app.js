622a36f915a813bc0da004950640fc41fd4e8809
Maximiliano Rendon <119466895+MaxRendon@users.noreply.github.com>
2024-05-02 10:54:41 -0700

    AgregaryModificar
    AgregaryModificar

--- proyecto/backend/app.js
+++ proyecto/backend/app.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let mysql = require('mysql');
 let cors = require('cors');
+const e = require('express');
 
 let app = express();
 app.use(express.json());
@@ -37,6 +38,68 @@ app.get('/api/clientes',(req,res)=>{
         }
     });
 })
+//Sleccionamos un cliente en especifico
+app.get('/api/clientes/:id',(req,res)=>{
+    conexion.query('SELECT * FROM clientes WHERE id=?',[req.params.id],(error,fila)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(fila);
+        }
+    })
+})
+
+app.delete('/api/clientes/:id',(req,res)=>{
+    let id = req.params.id;
+    conexion.query('DELETE FROM clientes WHERE id=?',[id],(error,filas)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(filas)
+        }
+    })
+});
+
+app.post('/api/clientes',(req,res)=>{
+    let data = {
+        id:req.body.id,
+        nombre:req.body.nombre,
+        apellido:req.body.apellido,
+        direccion:req.body.direccion,
+        telefono:req.body.telefono,
+        rfc:req.body.rfc,
+        curp:req.body.curp,
+        cp:req.body.cp
+    }
+    let sql = "INSERT INTO clientes SET ?";
+    conexion.query(sql,data,(error,resultado)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(resultado);
+        }
+    })
+});
+
+//Actualizar
+app.put('/api/clientes/:id',(req,res)=>{
+    let id = req.params.id;
+    let nombre=req.body.nombre;
+    let apellido=req.body.apellido;
+    let direccion=req.body.direccion;
+    let telefono=req.body.telefono;
+    let rfc=req.body.rfc;
+    let curp=req.body.curp;
+    let cp=req.body.cp;
+    let sql="UPDATE clientes SET nombre=?,apellido=?,direccion=?,telefono=?,rfc=?,curp=?,cp=? WHERE id=?";
+    conexion.query(sql,[nombre,apellido,direccion,telefono,rfc,curp,cp,id],(error,resultado)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(resultado);
+        }
+    })
+});
 
 //Encender servidor
 let puerto = 3000;
