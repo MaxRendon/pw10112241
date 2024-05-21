edb5f46bf559df8c3c955960bc15092c25c76ab8
Maximiliano Rendon <119466895+MaxRendon@users.noreply.github.com>
2024-05-21 10:54:34 -0700

    validaciones
    validaciones

--- proyecto/frontend/tienda/src/views/ClientesCreateView.vue
+++ proyecto/frontend/tienda/src/views/ClientesCreateView.vue
@@ -8,51 +8,93 @@
                 </div>
             </div>
             <div class="card-body">
-                <div class="mb-3">
-                    Id
-                    <input type="text" class="form-control" v-model="model.cliente.id">
-                </div>
-                <div class="mb-3">
-                    Nombre
-                    <input type="text" class="form-control" v-model="model.cliente.nombre">
-                </div>
-                <div class="mb-3">
-                    Apellido
-                    <input type="text" class="form-control" v-model="model.cliente.apellido">
-                </div>
-                <div class="mb-3">
-                    Direccion
-                    <input type="text" class="form-control" v-model="model.cliente.direccion">
-                </div>
-                <div class="mb-3">
-                    Telefono
-                    <input type="text" class="form-control" v-model="model.cliente.telefono">
-                </div>
-                <div class="mb-3">
-                    RFC
-                    <input type="text" class="form-control" v-model="model.cliente.rfc">
-                </div>
-                <div class="mb-3">
-                    CURP
-                    <input type="text" class="form-control" v-model="model.cliente.curp">
-                </div>
-                <div class="mb-3">
-                    CP
-                    <input type="text" class="form-control" v-model="model.cliente.cp">
-                </div>
-                <button class="btn btn-primary" @click="guardarCliente()">Guardar</button>
+                <Form :validation-schema="validatioSchema" @submit="ontTodoBien">
+                    <div class="mb-3">
+                        Id
+                        <Field name="id" id="id" type="number" class="form-control" v-model="model.cliente.id"/>
+                        <ErrorMessage name="id" class="errorValidacion"/> 
+                    </div>
+                    <div class="mb-3">
+                        Nombre
+                        <Field name="nombre" id="nombre" type="text" class="form-control" v-model="model.cliente.nombre"/>
+                        <ErrorMessage name="nombre" class="errorValidacion"/> 
+                    </div>
+                    <div class="mb-3">
+                        Apellido
+                        <Field name="apellido" id="apellido" type="text" class="form-control" v-model="model.cliente.apellido"/>
+                        <ErrorMessage name="apellido" class="errorValidacion"/> 
+                    </div>
+                    <div class="mb-3">
+                        Direccion
+                        <Field name="direccion" id="direccion" type="text" class="form-control" v-model="model.cliente.direccion"/>
+                        <ErrorMessage name="direccion" class="errorValidacion"/> 
+                    </div>
+                    <div class="mb-3">
+                        Telefono
+                        <Field name="telefono" id="telefono" type="text" class="form-control" v-model="model.cliente.telefono"/>
+                        <ErrorMessage name="telefono" class="errorValidacion"/> 
+                    </div>
+                    <div class="mb-3">
+                        RFC
+                        <Field name="rfc" id="rfc" type="text" class="form-control" v-model="model.cliente.rfc"/>
+                        <ErrorMessage name="rfc" class="errorValidacion"/> 
+                    </div>
+                    <div class="mb-3">
+                        CURP
+                        <Field name="curp" id="curp" type="text" class="form-control" v-model="model.cliente.curp"/>
+                        <ErrorMessage name="curp" class="errorValidacion"/> 
+                    </div>
+                    <div class="mb-3">
+                        CP
+                        <Field name="cp" id="cp" type="text" class="form-control" v-model="model.cliente.cp"/>
+                        <ErrorMessage name="cp" class="errorValidacion"/> 
+                    </div>
+                    <div class="mb-3">
+                        <button type="submit" class="btn btn-primary">Guardar</button>
+                    </div> 
+                </Form>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import axios from 'axios';
+    import axios from 'axios';
+    import { Field, Form,ErrorMessage} from 'vee-validate';
+    import { toTypedSchema } from '@vee-validate/zod';
+    import * as zod from 'zod';
+
 
     export default{
         name: 'ClientesCreate',
+        components: { Field, Form, ErrorMessage },
         data(){
+            const phoneRegex = new RegExp(
+                /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
+            );
+            const rfcRegex = new RegExp(
+                /^([a-z]{3,4})(\d{2})(\d{2})(\d{2})([0-9a-z]{3})$/i
+            );
+            const curpRegex = new RegExp(
+                /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9][12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/
+            );
+            const cpRegex = new RegExp(
+                /^[0-9]{5}$/
+            );
+            const validatioSchema = toTypedSchema(
+                zod.object({
+                    id: zod.number({message: 'Solo Numeros'}).int(),
+                    nombre: zod.string().min(1,{message: 'Requerido'}),  
+                    apellido: zod.string().min(1,{message: 'Requerido'}),  
+                    direccion: zod.string().min(1,{message: 'Requerido'}),  
+                    telefono: zod.string().regex(phoneRegex, 'Numero no Valido').min(10,{message: 'Minimo 10'}),
+                    rfc: zod.string().regex(rfcRegex,'RFC no valido'),
+                    curp: zod.string().regex(curpRegex,'CURP no valido'),
+                    cp: zod.string().regex(cpRegex,'Codigo postal no valido')      
+                })
+            )
             return{
+                validatioSchema,
                 mensaje: 0,
                 model:{
                     cliente: {
@@ -72,6 +114,9 @@ import axios from 'axios';
             this.mensaje = 1;
         },
         methods:{
+            onTodoBien(){
+                alert('Todo Validado')
+            },
             guardarCliente() {
                 axios.post('http://localhost:3000/api/clientes', this.model.cliente)
                 .then(res => {
@@ -96,3 +141,10 @@ import axios from 'axios';
         }
     }
 </script>
+
+<style scoped>
+    .errorValidacion{
+        color: red;
+        font-weight: bold;
+    }
+</style>
